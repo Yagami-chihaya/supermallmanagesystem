04069b772b0fe46b9f4127dc310a2c943f064012
Yagamichihaya <935760346@qq.com>
2021-09-08 12:02:43 +0800

    enter

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+const productPlugins = []
+if(process.env.NODE_ENV == 'production'){
+  productPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins:[
+    ...productPlugins
   ]
+
 }
